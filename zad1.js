86ada992f734284202a147c59357eb9647da2595
Jakub-K0p <Kopytowski.Jakub@zst.radom.pl>
2026-09-18 14:49:12 +0200

    zadanie 3

--- zad1.js
+++ zad1.js
@@ -10,3 +10,15 @@ document.writeln(`<p>Suma ${a} + ${b} + ${c} = ${suma}</p>`);
 // Zad2
 let avg = suma /3;
 document.writeln(`<p>Średnia: ${avg}</p>`);
+
+// Zad3
+a = parseFloat(prompt("Podaj pierwszą liczbę:"));
+b = parseFloat(prompt("Podaj drugą liczbę:"));
+ 
+suma = a + b;
+let roznica = a - b;
+let iloczyn = a * b;
+
+document.writeln(`<p>suma: ${a} + ${b} = ${suma}</p>`);
+document.writeln(`<p>roznica: ${a} - ${b} = ${roznica}</p>`);
+document.writeln(`<p>iloczyn:  ${a} * ${b} = ${iloczyn}</p>`);
