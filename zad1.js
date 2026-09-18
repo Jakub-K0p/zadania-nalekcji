2c3ae50e458d9fee55f55d4f3048377a7e74035b
Jakub-K0p <Kopytowski.Jakub@zst.radom.pl>
2026-09-18 14:41:10 +0200

    zad2

--- zad1.js
+++ zad1.js
@@ -1,3 +1,4 @@
+// Zad1
 let a = Number(prompt("Podaj pierwszą liczbę:"));
 let b = Number(prompt("Podaj drugą liczbę:"));
 let c = Number(prompt("Podaj trzecią liczbę:"));
@@ -6,3 +7,6 @@ let suma = a + b + c;
 //alert("Suma = " + suma);
 document.writeln(`<p>Suma ${a} + ${b} + ${c} = ${suma}</p>`);
  
+// Zad2
+let avg = suma /3;
+document.writeln(`<p>Średnia: ${avg}</p>`);
